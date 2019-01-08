1076c0f4ee8f45cd41fb4e985f30e094a06e702c
Dinah Deshane <dinahdeshane@gmail.com>
2019-01-08 15:04:36 -0500

    homework

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Landon#1",
+  database: "burgers_DB"
+});
+
+connection.connect(function(err) {
+  if (err)throw err;
+  console.log("connected as id:"+connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+    burger_name, 
+    devoured
+) VALUES
+(
+    "Burger",
+    0
+),
+(
+    "Cheese Burger",
+    0
+),
+(
+    "Bacon Cheese Burger",
+    0
+);
